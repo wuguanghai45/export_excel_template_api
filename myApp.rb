5c77f8cf6e4e395c274f768df5629e49b1f40788
walt <912788137@qq.com>
2017-04-13 15:20:38 +0800

    change project to data

--- myApp.rb
+++ myApp.rb
@@ -4,6 +4,6 @@ require File.expand_path(File.dirname(__FILE__) + '/lib/convert_to_excel')
 
 post '/' do
   @path = params[:template][:tempfile]
-  results = ConvertToExcel.new(@path, params[:object]).process
+  results = ConvertToExcel.new(@path, params[:data]).process
   send_file results.first, filename: "Customers.xls"
 end
